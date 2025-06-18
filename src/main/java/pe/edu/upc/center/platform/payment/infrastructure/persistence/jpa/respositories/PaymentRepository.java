ec37dd9cfa7eb25159c1fd8bc306cbdade73c55c
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-06-18 17:53:37 -0500

    feat: add repository interface

--- src/main/java/pe/edu/upc/center/platform/payment/infrastructure/persistence/jpa/respositories/PaymentRepository.java
+++ src/main/java/pe/edu/upc/center/platform/payment/infrastructure/persistence/jpa/respositories/PaymentRepository.java
@@ -1,4 +1,4 @@
-package pe.edu.upc.center.platform.payment.infrastructure.persistence.jpa;
+package pe.edu.upc.center.platform.payment.infrastructure.persistence.jpa.respositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +8,6 @@ import java.util.Optional;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
-    Optional<Payment> findAllByUserId(Long userId);
+    Optional<Payment> findByReservationId(Long reservationId);
+    Optional<Payment> findByStripePaymentId(String stripePaymentId);
 }
