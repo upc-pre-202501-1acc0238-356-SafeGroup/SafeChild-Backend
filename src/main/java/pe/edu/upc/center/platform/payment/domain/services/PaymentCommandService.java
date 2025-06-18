9a967758c49b97739d6c976cc5f7b9d2b1be9dd0
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-06-18 17:53:29 -0500

    feat: add services interface

--- src/main/java/pe/edu/upc/center/platform/payment/domain/services/PaymentCommandService.java
+++ src/main/java/pe/edu/upc/center/platform/payment/domain/services/PaymentCommandService.java
@@ -1,9 +1,14 @@
 package pe.edu.upc.center.platform.payment.domain.services;
 
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pe.edu.upc.center.platform.payment.domain.model.aggregates.Payment;
 import pe.edu.upc.center.platform.payment.domain.model.commands.CreatePaymentCommand;
 
-public interface PaymentCommandService {
+public interface PaymentCommandService{
     Payment handle(CreatePaymentCommand command);
+    PaymentIntent confirmPaymentIntent(String paymentIntentId) throws StripeException;
 }
 
--- src/main/java/pe/edu/upc/center/platform/payment/domain/services/PaymentQueryService.java
+++ src/main/java/pe/edu/upc/center/platform/payment/domain/services/PaymentQueryService.java
@@ -1,10 +1,14 @@
 package pe.edu.upc.center.platform.payment.domain.services;
 
 import pe.edu.upc.center.platform.payment.domain.model.aggregates.Payment;
-import pe.edu.upc.center.platform.payment.domain.model.queries.GetPaymentByUserQuery;
+import pe.edu.upc.center.platform.payment.domain.model.queries.GetAllPaymentsQuery;
+import pe.edu.upc.center.platform.payment.domain.model.queries.GetPaymentByReservationIdQuery;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PaymentQueryService {
-    List<Payment> handle(GetPaymentByUserQuery query);
+    Optional<Payment> handle(GetPaymentByReservationIdQuery query);
+    List<Payment> handle(GetAllPaymentsQuery query);
+
 }
