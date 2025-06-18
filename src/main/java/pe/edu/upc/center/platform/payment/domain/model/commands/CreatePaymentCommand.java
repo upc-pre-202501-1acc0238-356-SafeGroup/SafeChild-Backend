70b5d3453c27e559c6ab94476b24ed32c16a09ae
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-06-18 17:51:36 -0500

    feat: add command

--- src/main/java/pe/edu/upc/center/platform/payment/domain/model/commands/CreatePaymentCommand.java
+++ src/main/java/pe/edu/upc/center/platform/payment/domain/model/commands/CreatePaymentCommand.java
@@ -1,5 +1,7 @@
 package pe.edu.upc.center.platform.payment.domain.model.commands;
 
-public record CreatePaymentCommand(Long userId, Long reservationId, Long cardId) {
+import pe.edu.upc.center.platform.payment.domain.model.valueobjects.Currency;
+
+public record CreatePaymentCommand(Currency currency, Double amount, Long reservationId, String stripePaymentId) {
 }
 
