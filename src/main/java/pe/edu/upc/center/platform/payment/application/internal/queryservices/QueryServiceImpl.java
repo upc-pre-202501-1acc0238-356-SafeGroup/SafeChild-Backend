2b805858e1114d2ff3178d6f0ca1f6a07f58cbfd
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-06-18 20:09:59 -0500

    chore: update amount datatype to Long

--- src/main/java/pe/edu/upc/center/platform/payment/application/internal/queryservices/QueryServiceImpl.java
+++ src/main/java/pe/edu/upc/center/platform/payment/application/internal/queryservices/QueryServiceImpl.java
@@ -3,6 +3,7 @@ package pe.edu.upc.center.platform.payment.application.internal.queryservices;
 import org.springframework.stereotype.Service;
 import pe.edu.upc.center.platform.payment.domain.model.aggregates.Payment;
 import pe.edu.upc.center.platform.payment.domain.model.queries.GetAllPaymentsQuery;
+import pe.edu.upc.center.platform.payment.domain.model.queries.GetPaymentIntentIdQuery;
 import pe.edu.upc.center.platform.payment.domain.model.queries.GetPaymentByReservationIdQuery;
 import pe.edu.upc.center.platform.payment.domain.services.PaymentQueryService;
 import pe.edu.upc.center.platform.payment.infrastructure.persistence.jpa.respositories.PaymentRepository;
@@ -13,7 +14,11 @@ import java.util.Optional;
 @Service
 public class QueryServiceImpl implements PaymentQueryService {
 
-    private PaymentRepository paymentRepository;
+    private final PaymentRepository paymentRepository;
+
+    public QueryServiceImpl(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
+    }
 
     @Override
     public Optional<Payment> handle(GetPaymentByReservationIdQuery query) {
@@ -24,4 +29,11 @@ public class QueryServiceImpl implements PaymentQueryService {
     public List<Payment> handle(GetAllPaymentsQuery query) {
         return this.paymentRepository.findAll();
     }
+
+    @Override
+    public Optional<Payment> handle(GetPaymentIntentIdQuery query) {
+        return this.paymentRepository.findByStripePaymentId(query.paymentIntentId());
+    }
+
+
 }
