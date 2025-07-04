3b3094d718e40aa7fceecee9f33d886bea1a3947
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-07-04 04:11:52 -0500

    chore(schedule): update CreateScheduleCommand to include availableDate

--- src/main/java/pe/edu/upc/center/platform/schedules/domain/model/commands/CreateScheduleCommand.java
+++ src/main/java/pe/edu/upc/center/platform/schedules/domain/model/commands/CreateScheduleCommand.java
@@ -1,7 +1,9 @@
 package pe.edu.upc.center.platform.schedules.domain.model.commands;
 
+import java.time.LocalDate;
+
 public record CreateScheduleCommand(
         Long caregiverId,
-        boolean isAvailable
+        LocalDate availableDate
 ) {
 }
