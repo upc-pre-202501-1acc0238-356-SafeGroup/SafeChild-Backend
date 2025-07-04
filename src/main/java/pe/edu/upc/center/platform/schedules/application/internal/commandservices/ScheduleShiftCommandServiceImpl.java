4bc2f8713d7ac8577f5d1923f32acfe2b7b247b1
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-07-04 04:17:28 -0500

    fix(schedule): fix update func for saving info

--- src/main/java/pe/edu/upc/center/platform/schedules/application/internal/commandservices/ScheduleShiftCommandServiceImpl.java
+++ src/main/java/pe/edu/upc/center/platform/schedules/application/internal/commandservices/ScheduleShiftCommandServiceImpl.java
@@ -1,11 +1,7 @@
 package pe.edu.upc.center.platform.schedules.application.internal.commandservices;
-
-
-import pe.edu.upc.center.platform.schedules.domain.model.aggregates.Schedule;
 import pe.edu.upc.center.platform.schedules.domain.model.commands.UpdateShiftAvailabilityCommand;
 import pe.edu.upc.center.platform.schedules.domain.model.entities.ScheduleShift;
 import pe.edu.upc.center.platform.schedules.domain.services.ScheduleShiftCommandService;
-import pe.edu.upc.center.platform.schedules.infrastructure.persistence.jpa.repositories.ScheduleRepository;
 import org.springframework.stereotype.Service;
 import pe.edu.upc.center.platform.schedules.infrastructure.persistence.jpa.repositories.ScheduleShiftRepository;
 
@@ -15,56 +11,29 @@ import java.util.Optional;
 public class ScheduleShiftCommandServiceImpl implements ScheduleShiftCommandService {
 
     private final ScheduleShiftRepository scheduleShiftRepository;
-    private final ScheduleRepository scheduleRepository;
 
-    public ScheduleShiftCommandServiceImpl(ScheduleShiftRepository scheduleShiftRepository, ScheduleRepository scheduleRepository) {
+    public ScheduleShiftCommandServiceImpl(ScheduleShiftRepository scheduleShiftRepository) {
         this.scheduleShiftRepository = scheduleShiftRepository;
-        this.scheduleRepository = scheduleRepository;
     }
 
 
     @Override
     public Optional<ScheduleShift> handle(UpdateShiftAvailabilityCommand command) {
-        var existsId = scheduleShiftRepository.existsById(command.shiftId());
-        if (!existsId) {
-            throw new IllegalArgumentException("Availability with id %s not found".formatted(command.shiftId()));
+
+        Optional<ScheduleShift> optionalScheduleShift = scheduleShiftRepository.findById(command.shiftId());
+
+        if (optionalScheduleShift.isEmpty()) {
+            throw new IllegalArgumentException("Schedule shift with id %s not found".formatted(command.shiftId()));
         }
 
+        ScheduleShift scheduleShift = optionalScheduleShift.get();
 
+        scheduleShift.setAvailable(command.available());
 
-        var result = scheduleShiftRepository.findByScheduleIdAndAvailable(command.scheduleId(), command.shiftId());
-        var scheduleAvailabilityToUpdate = result.get();
+        scheduleShiftRepository.save(scheduleShift);
 
-        // Actualizar disponibilidad del slot
-        scheduleAvailabilityToUpdate.(command.free());
-        var updatedScheduleAvailability = scheduleAvailabilityRepository.save(scheduleAvailabilityToUpdate);
-
-        // Obtener el Schedule asociado
-        Schedule schedule = updatedScheduleAvailability.getSchedule();
-
-        // Verificar si todos los ScheduleAvailability están en false
-        boolean allUnavailable = schedule.getScheduleAvailabilities()
-                .stream()
-                .noneMatch(ScheduleAvailability::isFree); // ninguno es libre
-
-        // Actualizar isAvailable si corresponde
-        schedule.setAvailable(!allUnavailable);
-        scheduleRepository.save(schedule);
-
-        return Optional.of(schedule);
+        return Optional.of(scheduleShift);
     }
-
-
-//    public Reservation handle(UpdateReservationStatusCommand command) {
-//        Reservation reservation = reservationRepository.findById(command.reservationId())
-//                .orElseThrow(() -> new IllegalArgumentException("Reservation not found"));
-//
-//        reservation.setStatus(ReservationStatus.valueOf(command.status()));
-//        return reservationRepository.save(reservation);
-//    }
-
-
-
 
 
 }
