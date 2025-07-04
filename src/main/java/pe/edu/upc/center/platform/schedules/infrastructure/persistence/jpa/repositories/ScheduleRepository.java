07e1b5f0f0a0aaf056edbf456acd5d93128ac622
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-07-03 20:35:41 -0500

    feat(schedule): add repositories

---
+++ src/main/java/pe/edu/upc/center/platform/schedules/infrastructure/persistence/jpa/repositories/ScheduleRepository.java
@@ -0,0 +1,18 @@
+package pe.edu.upc.center.platform.schedules.infrastructure.persistence.jpa.repositories;
+
+
+import pe.edu.upc.center.platform.schedules.domain.model.aggregates.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ScheduleRepository extends JpaRepository<Schedule, Long>  {
+
+    Optional<Schedule> findById(Long id);
+
+    List<Schedule> findScheduleByAdvisorId(Long advisorId);
+
+}
