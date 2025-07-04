1bff622116d01f3f75cc3f53449ac33157197f2b
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-07-04 04:15:24 -0500

    feat(schedule): enhance ScheduleShift entity with Lombok annotations and update relationship mapping

--- src/main/java/pe/edu/upc/center/platform/schedules/domain/model/entities/ScheduleShift.java
+++ src/main/java/pe/edu/upc/center/platform/schedules/domain/model/entities/ScheduleShift.java
@@ -1,24 +1,41 @@
 package pe.edu.upc.center.platform.schedules.domain.model.entities;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import pe.edu.upc.center.platform.schedules.domain.model.aggregates.Schedule;
 import pe.edu.upc.center.platform.schedules.domain.model.valueobjects.Shift;
 import pe.edu.upc.center.platform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
 
 @Entity
-public class ScheduleShift extends AuditableAbstractAggregateRoot<ScheduleShift> {
+@Getter
+@Setter
+public class ScheduleShift {
 
+    @Id
+    @Getter
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @ManyToOne
     @JoinColumn(name = "schedule_id")
-    Schedule scheduleId;
+    @JsonBackReference
+    Schedule schedule;
 
     @Enumerated(EnumType.STRING)
     Shift shift;
 
     boolean available;
 
-    public ScheduleShift() {
+    public ScheduleShift(Long id, Schedule schedule, Shift shift, boolean available) {
+        this.id = id;
+        this.schedule = schedule;
+        this.shift = shift;
+        this.available = available;
     }
 
+    public ScheduleShift() {
+
+    }
 }
