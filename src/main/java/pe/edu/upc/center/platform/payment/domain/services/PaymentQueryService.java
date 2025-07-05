8c43eee7c61a22334b36363d574bd2fc6f92c8eb
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-07-05 01:19:58 -0500

    feat(payments): add command service, query service and repository interfaces

--- src/main/java/pe/edu/upc/center/platform/payment/domain/services/PaymentQueryService.java
+++ src/main/java/pe/edu/upc/center/platform/payment/domain/services/PaymentQueryService.java
@@ -1,10 +1,7 @@
 package pe.edu.upc.center.platform.payment.domain.services;
 
 import pe.edu.upc.center.platform.payment.domain.model.aggregates.Payment;
-import pe.edu.upc.center.platform.payment.domain.model.queries.GetAllPaymentsQuery;
-import pe.edu.upc.center.platform.payment.domain.model.queries.GetByIdQuery;
-import pe.edu.upc.center.platform.payment.domain.model.queries.GetPaymentIntentIdQuery;
-import pe.edu.upc.center.platform.payment.domain.model.queries.GetPaymentByReservationIdQuery;
+import pe.edu.upc.center.platform.payment.domain.model.queries.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,4 +11,6 @@ public interface PaymentQueryService {
     List<Payment> handle(GetAllPaymentsQuery query);
     Optional<Payment> handle(GetPaymentIntentIdQuery query);
     Optional<Payment> handle(GetByIdQuery query);
+    List<Payment> handle(GetPaymentsByCaregiverIdAndPaymentStatusQuery query);
+    List<Payment> handle(GetPaymentsByCaregiverIdQuery query);
 }
--- src/main/java/pe/edu/upc/center/platform/payment/infrastructure/persistence/jpa/respositories/PaymentRepository.java
+++ src/main/java/pe/edu/upc/center/platform/payment/infrastructure/persistence/jpa/respositories/PaymentRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.center.platform.payment.domain.model.aggregates.Payment;
+import pe.edu.upc.center.platform.payment.domain.model.valueobjects.PaymentStatus;
 import pe.edu.upc.center.platform.payment.domain.model.valueobjects.ReservationId;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,5 +16,14 @@ public interface PaymentRepository extends JpaRepository<Payment, Long> {
     Optional<Payment> findByStripePaymentId(String stripePaymentId);
     boolean existsByStripePaymentId(String stripePaymentId);
 
-    Optional<Payment> findPaymentByIdyId(Long id);
+    Optional<Payment> findPaymentById(Long id);
+   // List<Payment> findPaymentsByCaregiverId(Long id);
+    List<Payment> findPaymentByReservation_CaregiverId(Long id);
+    //List<Payment> findPaymentsByCaregiverIdAndPaymentStatus(Long caregiverId, PaymentStatus paymentStatus);
+    List<Payment> findPaymentByReservation_CaregiverIdAndPaymentStatus(Long caregiverId, PaymentStatus paymentStatus);
+
+
+
+
+
 }
