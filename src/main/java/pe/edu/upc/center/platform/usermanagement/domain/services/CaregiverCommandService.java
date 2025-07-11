a60fa64149b1c4d4dbfb4b2dbd68b1128af1d1b7
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-07-11 15:35:19 -0500

    chore: add create command

--- src/main/java/pe/edu/upc/center/platform/usermanagement/domain/services/CaregiverCommandService.java
+++ src/main/java/pe/edu/upc/center/platform/usermanagement/domain/services/CaregiverCommandService.java
@@ -4,10 +4,12 @@ import pe.edu.upc.center.platform.usermanagement.domain.model.aggregates.Caregiv
 import pe.edu.upc.center.platform.usermanagement.domain.model.entities.CaregiverSchedule;
 import pe.edu.upc.center.platform.usermanagement.domain.model.commands.*;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 public interface CaregiverCommandService {
     Caregiver handle(CreateCaregiverCommand command);
+    Optional<Caregiver> handle(CreateCaregiverByIdCommand command);
 
     CaregiverSchedule handle(CreateCaregiverScheduleCommand command);
 
--- src/main/java/pe/edu/upc/center/platform/usermanagement/domain/services/TutorCommandService.java
+++ src/main/java/pe/edu/upc/center/platform/usermanagement/domain/services/TutorCommandService.java
@@ -1,6 +1,7 @@
 package pe.edu.upc.center.platform.usermanagement.domain.services;
 
 import pe.edu.upc.center.platform.usermanagement.domain.model.aggregates.Tutor;
+import pe.edu.upc.center.platform.usermanagement.domain.model.commands.CreateTutorByIdCommand;
 import pe.edu.upc.center.platform.usermanagement.domain.model.commands.CreateTutorCommand;
 import pe.edu.upc.center.platform.usermanagement.domain.model.commands.DeleteTutorCommand;
 import pe.edu.upc.center.platform.usermanagement.domain.model.commands.UpdateTutorCommand;
@@ -10,5 +11,6 @@ import java.util.Optional;
 public interface TutorCommandService {
     Long handle(CreateTutorCommand command);
     Optional<Tutor> handle(UpdateTutorCommand command);
+    Optional<Tutor> handle(CreateTutorByIdCommand command);
     void handle(DeleteTutorCommand command);
 }
